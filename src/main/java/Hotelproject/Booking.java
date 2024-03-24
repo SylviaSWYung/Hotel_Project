fb117378a0822b3ab22277e3191a82b0de687511
Sylvia Yung <syung314@gmail.com>
2024-03-24 15:29:12 +0100

    Lagt til hotelproject samt 4 java filer

---
+++ src/main/java/Hotelproject/Booking.java
@@ -0,0 +1,12 @@
+package Hotelproject;
+
+import java.util.Date;
+
+public class Booking {
+    private Date startDate; 
+    private Date endDate; 
+    private String sted; 
+    private int antallPersoner;
+
+
+}
