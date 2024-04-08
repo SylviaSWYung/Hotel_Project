4ae1820f90fc24114dd12e1ebb790eb5650d5a01
Jennica Duong <jennicad@online.ntnu.no>
2024-04-08 13:07:06 +0200

    endringer

---
+++ src/main/java/Hotelproject/vippsBetaling.java
@@ -0,0 +1,30 @@
+package Hotelproject;
+
+import java.io.IOException;
+
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+
+public class vippsBetaling implements Betaling {
+
+    public void gjennomforbetalingvipps(int price, int guestInput, int totalPrice) throws IOException {
+        try {
+            totalPrice = Betaling.calculateTotalPrice(price, guestInput);
+            Alert alert = new Alert(AlertType.CONFIRMATION);
+            alert.setTitle("Confirmation");
+            alert.setHeaderText("You have successfully paid the total amount of " + totalPrice + " by using Vipps");
+            alert.setContentText("Booking information is updated.");
+            
+            alert.showAndWait();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+
+            Alert alert = new Alert(AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText("Could not pay the amount " + totalPrice + "by using Vipps");
+            alert.setContentText("A problem occurred while trying to pay: " + e.getMessage());
+            
+            alert.showAndWait();
+        }
+    }
